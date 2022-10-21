ff31bab7d8daf929febd95b6f6b6068ad78e8e29
ebayboy123 <107748760+ebayboy123@users.noreply.github.com>
2022-10-21 19:37:36 +0200

    working v1

--- Automat.java
+++ Automat.java
@@ -12,28 +12,23 @@ public class Automat {
         try {
 
         int num_of_tickets;
-        num_of_tickets = 0;
-        Payedmoney = 0;
 
-        
+        Scanner input = new Scanner(System.in);
+        String choosen_ticket;
+        String traincard;
+        Integer age;
+        Double inserted_coins;
+        Double result = null;
 
         while (true) {
 
-            // scanner
-            Scanner input = new Scanner(System.in);
-            String choosen_ticket;
-            String traincard;
-            Integer age;
-            Double inserted_coins;
-            Double Payedmoney;
-            Double result;
-            List < String > avalible_buying_options = new ArrayList < String > ();
+            List<String> avalible_buying_options = new ArrayList<String>();
             avalible_buying_options.add("a");
             avalible_buying_options.add("b");
-            avalible_buying_options.add("b");
+            avalible_buying_options.add("c");
 
 
-            List < Double > possible_coins = new ArrayList <Double> ();
+            List<Double> possible_coins = new ArrayList<Double>();
             possible_coins.add(0.01);
             possible_coins.add(0.02);
             possible_coins.add(0.05);
@@ -57,123 +52,115 @@ public class Automat {
 
                 // number of tickets
                 System.out.println("how many tickets would you like to buy");
-                num_of_tickets += input.nextInt();
+                num_of_tickets = input.nextInt();
 
-                if (num_of_tickets <= 0) {
+                if (num_of_tickets > 0) {
+
+                    System.out.println("do you have a train card y/n");
+                    traincard = input.next();
+
+                    System.out.println("how old are you: ");
+                    age = input.nextInt();
+
+                    switch (choosen_ticket) {
+
+                        case "a":
+
+                            if (traincard.equals("y")) {
+
+                                if (age < 18) {
+                                    System.out.println("you have to pay " + (result * 0.5) + "€");
+                                } else {
+                                    result = (2.50 * (num_of_tickets - (0.2 * num_of_tickets)));
+                                    System.out.println("you have to pay " + result + "€");
+                                }
+                            } else {
+
+                                if (age < 18) {
+                                    System.out.println("you have to pay " + (result * 0.5) + "€");
+                                } else {
+                                    result = (2.50 * num_of_tickets);
+                                    System.out.println("you have to pay " + result + "€");
+                                }
+                            }
+                            break;
+
+                        case "b":
+                            if (traincard.equals("y")) {
+
+                                if (age < 18) {
+                                    System.out.println("you have to pay " + (result * 0.5) + "€");
+                                } else {
+                                    result = (3.70 * (num_of_tickets - (0.2 * num_of_tickets)));
+                                    System.out.println("you have to pay " + result + "€");
+                                }
+                            } else {
+
+
+                                if (age < 18) {
+                                    System.out.println("you have to pay " + (result * 0.5) + "€");
+                                } else {
+                                    result = (3.70 * num_of_tickets);
+                                    System.out.println("you have to pay " + result + "€");
+                                }
+                            }
+                            break;
+
+                        case "c":
+                            if (traincard.equals("y")) {
+
+                                if (age < 18) {
+                                    System.out.println("you have to pay " + (result * 0.5) + "€");
+                                } else {
+                                    result = (5.20 * (num_of_tickets - (0.2 * num_of_tickets)));
+                                    System.out.println("you have to pay " + result + "€");
+                                }
+                            } else {
+
+                                if (age < 18) {
+                                    System.out.println("you have to pay " + (result * 0.5) + "€");
+                                } else {
+                                    result = (5.20 * num_of_tickets);
+                                    System.out.println("you have to pay " + result + "€");
+                                }
+                            }
+
+                            break;
+
+                    }
+                } else {
                     System.out.println("you have to buy at least 1 ticket");
                 }
 
-                System.out.println("do you have a train card y/n");
-                traincard = input.next();
+            }
 
-                System.out.println("how old are you: ");
-                age = input.nextInt();
+            Double Payed_money = null;
+            Payed_money = 0.0;
 
-                switch (choosen_ticket) {
-
-                    case "a":
-
-                        if (traincard.equals("y")) {
-                            result = (2.50 * (num_of_tickets - (0.2 * num_of_tickets)));
-
-                            if (age < 18) {
-                                System.out.println("you have to pay " + (result * 0.5) + "€");
-                            }
-
-                            else {
-                                System.out.println("you have to pay " + result + "€");
-                            }
-                        }
-                        else {
-                            result = (2.50 * num_of_tickets);
-
-                            if (age < 18) {
-                                System.out.println("you have to pay " + (result * 0.5) + "€");
-                            }
-
-                            else {
-                                System.out.println("you have to pay " + result + "€");
-                            }
-                        }
-                        break;
-
-                    case "b":
-                        if (traincard.equals("y")) {
-                            result = (3.70 * (num_of_tickets - (0.2 * num_of_tickets)));
-
-                            if (age < 18) {
-                                System.out.println("you have to pay " + (result * 0.5) + "€");
-                            }
-
-                            else {
-                                System.out.println("you have to pay " + result + "€");
-                            }
-                        }
-                        else {
-                            result = (3.70 * num_of_tickets);
-
-                            if (age < 18) {
-                                System.out.println("you have to pay " + (result * 0.5) + "€");
-                            }
-
-                            else {
-                                System.out.println("you have to pay " + result + "€");
-                            }
-                        }
-                        break;
-
-                    case "c":
-                        if (traincard.equals("y")) {
-                            result = (5.20 * (num_of_tickets - (0.2 * num_of_tickets)));
-
-                            if (age < 18) {
-                                System.out.println("you have to pay " + (result * 0.5) + "€");
-                            }
-
-                            else {
-                                System.out.println("you have to pay " + result + "€");
-                            }
-                        }
-                        else {
-                            result = (5.20 * num_of_tickets);
-
-                            if (age < 18) {
-                                System.out.println("you have to pay " + (result * 0.5) + "€");
-                            }
-
-                            else {
-                                System.out.println("you have to pay " + result + "€");
-                            }
-                        }
-
-                        break;
-
-                        
-                    }
-
-                }
-
-            while (payedmoney < result) {
-
-            System.out.print("please insert coins (0.01, 0.02, 0.05, 0.10, 0.20, 0.50, 1, 2): ");
-            inserted_coins = input.nextDouble();
+            while (Payed_money < result) {
+                System.out.print("please insert coins (0.01, 0.02, 0.05, 0.10, 0.20, 0.50, 1.0, 2.0): ");
+                inserted_coins = input.nextDouble();
 
                 if (possible_coins.contains(inserted_coins)) {
-                    
+                    Payed_money = (inserted_coins += Payed_money);
+                    System.out.println("inserted " + inserted_coins + "€");
+                    System.out.println("left to pay " + (result - Payed_money) + "€");
+                } else {
+                    System.out.print("you cant use those coins \n");
                 }
 
             }
+
+            System.out.println("you have payed too much, \nthere for you will get a voucher for you next purchase: ");
                 System.out.println("Good Bye");
-            
+
+                num_of_tickets = 0;
+                Thread.sleep(4000);
 
 
-
-            num_of_tickets = 0;
-            Thread.sleep(4000);
         }
-
         } catch (Exception e) {
-            System.out.println("something went wrong... \n please retry again");
+            System.out.println("something went wrong... \nplease retry again");
         }
     }
 }
